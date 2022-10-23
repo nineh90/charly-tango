488bf7e500dc236ce2bd5ab65dcaebc2871c3e2f
z0mb1 <you@example.com>
2022-10-23 20:42:09 +0200

    memberlist incomplete

---
+++ assets/js/memberlist.js
@@ -0,0 +1,104 @@
+let loadedData;
+let allMemberList;
+
+window.onload = function () { 
+    waitForMemberlist();
+}
+
+function waitForMemberlist() {
+    setTimeout(() => {
+        allMemberList = document.getElementById('memberlist-container'); 
+        if (allMemberList) {
+            init();
+        } else {
+            waitForMemberlist();
+        }
+    });
+}
+
+async function init(){
+    let JSON = '../assets/json/member.json';
+    let responseFromJSON = await fetch(JSON);
+    loadedData = await responseFromJSON.json();
+    allMemberList.styl
+    forLoopCountryList();
+    renderHeadLine(); 
+}
+function renderHeadLine(){
+    let headLine = document.getElementById('headLine');
+    if (headLine){
+        headLine.innerHTML = `Mitgliederliste`;
+    }
+} 
+
+function forLoopCountryList(){ 
+    allMemberList.innerHTML = "";
+    for (let i = 0; i < loadedData.length; i++) {
+        const countryImage = loadedData[i]['flag'];
+        const countryName = loadedData[i]['country'];
+        const preFix = loadedData[i]['prefix'];
+        renderCountryList(countryImage, countryName, preFix, i);
+
+    }
+ 
+}   
+
+function renderCountryList(countryImage, countryName, preFix, i){
+    allMemberList.innerHTML += `
+                                <div onclick="openDetailview(${i})" class="country-card">
+                                    <div id="${countryName.toLowerCase()}" class="country-card-wrapper txt-center">
+                                        <h3>
+                                            ${countryName.toUpperCase()}
+                                        </h3>
+                                            <span class="fi fi-${countryImage} flag-icon"></span>
+                                        <p class="prefix-wrapper">
+                                            "${preFix}"
+                                        </p>
+                                        
+                                    </div>
+                                </div>
+                                `;
+}
+
+function openDetailview(i){
+    allMemberList.innerHTML = '';
+    allMemberList.style.display = 'none';
+    let memberJSON = loadedData[i]["member"];
+    generateTableCurrentCountry();
+    for (let j = 0; j < memberJSON.length; j++) {
+        const callsign = memberJSON[j]['callsign'];
+        const name = memberJSON[j]['name'];
+        const city = memberJSON[j]['city'];
+        const status = memberJSON[j]['status'];
+        renderMemberListCurrentCountry(callsign, name, city, status);        
+        if (j == memberJSON.length -1) {
+            allMemberList.style.display = 'flex';
+        }
+    }
+}
+
+function generateTableCurrentCountry(){
+    allMemberList.innerHTML = ` 
+                                    <table class="txt-center mobile-table">
+                                        <thead>
+                                            <tr>
+                                                <th>Rufzeichen</th>
+                                                <th>Name</th>
+                                                <th>Stadt</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="currentMemberList"></tbody>    
+                                    </table>  `;
+                             
+                    
+}
+
+function renderMemberListCurrentCountry(callsign, name, city, status){
+    currentMemberList.innerHTML += `<tr>
+                                        <td>${callsign}</td>
+                                        <td>${name.toUpperCase()}</td>
+                                        <td>${city.toUpperCase()}</td>
+                                        <td>${status.toUpperCase()}</td>
+                                    </tr>`
+}
